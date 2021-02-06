8e987119aa44d9864f18f3087cfea5bbcbb87672
shoeb <shoebsd31@gmail.com>
2021-02-06 17:58:03 +0100

    memoization of can sum problem

---
+++ can sum/cansum.js
@@ -0,0 +1,20 @@
+const canSum = (targetSum, numbers, memo={}) => {
+    if(targetSum in memo ) return memo[targetSum];
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+    for (const num of numbers) {
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum]=true;
+            return memo[targetSum];
+        };
+    }
+    memo[targetSum]=false;
+    return memo[targetSum];
+}
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7,[5,3,4,7]));
+console.log(canSum(7,[4,2]));
+console.log(canSum(8,[2,3,5]));
+console.log(canSum(300,[7,14]));
